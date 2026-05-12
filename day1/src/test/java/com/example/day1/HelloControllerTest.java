15203cc2a6ffc7f818f4e4618cfae7381139a343
up1 <somkiat.p@gmail.com>
2026-05-12 12:17:34 +0700

    Enable virtual thread + add test

---
+++ day1/src/test/java/com/example/day1/HelloControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.day1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class HelloControllerTest {
+
+    @Test
+    void sayHi() {
+    }
+}
