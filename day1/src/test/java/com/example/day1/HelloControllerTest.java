c6ba4476fa9a4430c905af4467847982ba05d71f
up1 <somkiat.p@gmail.com>
2026-05-12 16:42:13 +0700

    Mock with mockito

--- day1/src/test/java/com/example/day1/HelloControllerTest.java
+++ day1/src/test/java/com/example/day1/HelloControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.day1;
 
+import com.example.day1.dip.GenerateIdService;
+import com.example.day1.dip.XXXRandom;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +9,11 @@ import org.springframework.boot.resttestclient.TestRestTemplate;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestRestTemplate
@@ -34,8 +39,28 @@ class HelloControllerTest {
         assertEquals("Report V2", result);
     }
 
+//    @MockitoBean
+//    GenerateIdService stub;
+
+//    @Test
+//    void test03() {
+//        // Arrange
+//        when(stub.process()).thenReturn("DEMO-5");
+//
+//        // Act
+//        String result = restTemplate.getForObject("/random", String.class);
+//        // Assert
+//        assertEquals("DEMO-5", result);
+//    }
+
+    @MockitoBean
+    XXXRandom stub;
+
     @Test
-    void test03() {
+    void test04() {
+        // Arrange
+        when(stub.get(anyInt())).thenReturn(5);
+
         // Act
         String result = restTemplate.getForObject("/random", String.class);
         // Assert
