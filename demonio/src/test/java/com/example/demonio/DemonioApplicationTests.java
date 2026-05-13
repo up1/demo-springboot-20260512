eb9fdc104ce3cece6d92467733354f1f526e4c20
up1 <somkiat.p@gmail.com>
2026-05-13 15:05:52 +0700

    Demo with Spring Reactive Web

---
+++ demonio/src/test/java/com/example/demonio/DemonioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demonio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
